97dc42588f7ce64503815a31a2019d85d7acf08d
Shushpin <denusveluchko2017@gmail.com>
2026-02-22 18:31:26 +0200

    permit public access to actuator metrics for Prometheus

--- doc-classifier/src/main/java/com/diploma/doc_classifier/config/SecurityConfig.java
+++ doc-classifier/src/main/java/com/diploma/doc_classifier/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
                                                    CustomOAuth2UserService customOAuth2UserService) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/login", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/", "/login", "/css/**", "/js/**", "/images/**", "/actuator/").permitAll()
                         .anyRequest().authenticated()
                 )
                 // Старий добрий вхід через логін/пароль
